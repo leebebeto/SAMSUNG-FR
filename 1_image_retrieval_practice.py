9548a3d27cc66b9b8e5bb206039e97be9847fe97
bombs <bluewhale8202@gmail.com>
2020-08-06 18:39:19 +0000

    python version added

---
+++ 1_image_retrieval_practice.py
@@ -0,0 +1,327 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Image Retrieval 실습
+
+# In[ ]:
+
+
+# Import libraries
+import cv2
+import os
+import numpy as np
+import numpy.linalg as LA
+import time
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from skimage.transform import resize
+from PIL import Image
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+import torchvision
+import torchvision.transforms as transforms
+
+import warnings
+warnings.filterwarnings('ignore')
+os.environ['CUDA_VISIBLE_DEVICES']='1'
+
+
+# 
+# ## Feature Extractor (Color)
+
+# In[ ]:
+
+
+#  extract 3D HSV color histogram from images
+class Color_Extractor:
+    def __init__(self, bins):
+        # store # of bins for histogram
+        self.bins = bins
+
+    # convert RGB to HSV and 
+    # initialize features to quantify and represent the image
+    def describe(self, image):
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        features = []
+
+        # grab dimensions and computer center of image
+        # from beginning 0 to end-1 = 1, i.e. shape[0] & shape[1]
+        (h, w) = image.shape[:2]
+        (cx, cy) = (int(w * 0.5), int(h * 0.5))
+
+        # divide image into top-left, top-right, bottom-right, bottom-left corner segments as mask
+        segments = [(0,cx,0,cy), (0,cx,cy,h), (cx,w,cy,h), (cx,w,0,cy)]
+
+        # construct an elliptical mask representing the center of the image
+        (axesX, axesY) = (int(w * 0.75 / 2), int(h * 0.75 / 2))
+        ellipse_mask = np.zeros(image.shape[:2], dtype = "uint8")
+        cv2.ellipse(ellipse_mask, (cx, cy), (axesX, axesY), 0, 0, 360, (255, 255, 255), -1)
+
+        # loop over mask corners
+        for seg in segments:
+            # construct mask for each corner by np.zeros()
+            corner_mask = np.zeros(image.shape[:2], dtype = 'uint8')
+            # draw rectangle mask on corner_mask object
+            corner_mask[seg[0]:seg[1], seg[2]:seg[3]] = 255
+            corner_mask = cv2.subtract(corner_mask, ellipse_mask)
+
+            # extract hsv histogram	from segment of image with mask	
+            hist = self.histogram(image, corner_mask)
+
+            # update feature vector
+            features.extend(hist)
+
+        # extract hsv histogram from ellipse with mask
+        hist_ellipse = self.histogram(image, ellipse_mask)
+        features.extend(hist_ellipse)
+
+        return ???
+    
+    # Calculate the histogram of the masked region of the image
+    def histogram(self,image, mask):
+        # use number of bins per channel; 
+        hist = cv2.calcHist([image], [0,1,2], mask, self.bins, [0, 256, 0, 256, 0, 256])
+        
+        # normalize histogram to obtain scale invariance
+        hist = cv2.normalize(hist, None).flatten()
+    
+        return hist
+
+
+# In[ ]:
+
+
+def extract_color_features(path):
+    start_time = time.time()
+    
+    # Create a list to append feature vectors to
+    features = []
+    list_imgs_names = os.listdir(path)
+    color_extractor = Color_Extractor((8, 8, 8))
+    image_all = []
+    
+    # Iterate through the list of images
+    for img_name in list_imgs_names:        
+        # Read in each one by one
+        img_path = os.path.join(path, img_name)
+        image = mpimg.imread(img_path) # Read the images
+        image = ???(image, (224, 224), mode='constant') # Resize the images
+        image = (image * 255).astype(np.uint8)
+        feature_image = np.copy(image)
+        
+        feature = np.array(color_extractor.describe(feature_image))
+        feature = feature / LA.norm(???) # Feature Normalization
+        features.append(feature)
+        image_all.append(img_name)
+    
+    time_elapsed = ???
+    
+    print('Feature extraction complete in {:.02f}s'.format(time_elapsed % 60))
+    
+    # Return list of feature vectors
+    return np.array(features), image_all
+
+
+# In[ ]:
+
+
+def test_color_features():
+    print('Extract features from data')
+    path = './data'
+    feats, image_list = extract_color_features(path)
+
+    print('Extract features from query image')
+    test = './test'
+    feat_single, image = extract_color_features(test)
+    
+    # Calculate the scores
+    scores  = ???(feat_single, feats.T)  # 스코어 구하기
+    sort_ind = ???(scores)[0][::-1]      # 스코어 정렬하기
+    scores = scores[0, sort_ind]
+
+    # Show the results
+    maxres = 10
+    imlist = [image_list[index] for i, index in enumerate(sort_ind[0:maxres])]
+    print ("top %d images in order are: " %maxres, imlist)
+
+    fig=plt.figure(figsize=(16, 10))
+    for i in range(len(imlist)):
+        sample = imlist[i]
+        img = mpimg.imread('./data' + '/' + sample)
+        ax = fig.add_subplot(2, 5, i+1)
+        ax.autoscale()
+        plt.tight_layout()
+        plt.imshow(img, interpolation='nearest')
+        ax.set_title('{:.3f}%'.format(scores[i]))
+        ax.axis('off')
+    plt.show()
+
+
+# In[ ]:
+
+
+test_color_features()
+
+
+# ## Feature Extractor (VGG19)
+
+# In[ ]:
+
+
+class VGG19(nn.Module):
+    def __init__(self):
+        super(VGG19, self).__init__()
+        self.vgg19 = torchvision.models.vgg19(pretrained = True) # vgg 19 model is imported
+        #print(vgg19)
+        self.vgg19.classifier = self.vgg19.classifier[0:4]
+        
+    def forward(self, x):
+        out = self.vgg19(x)
+        return out
+
+# Set our model with pre-trained model 
+vgg19 = VGG19().cuda()
+
+
+# ## Feature Extractor (ResNet)
+
+# In[ ]:
+
+
+# Create the network to extract the features
+class ResNet(nn.Module):
+    def __init__(self):
+        super(ResNet, self).__init__()
+        resnet = torchvision.models.resnet50(pretrained = True) # resnet 50 model is imported
+        
+        #print(resnet)
+        self.conv1 = resnet.conv1
+        self.bn1 = resnet.bn1
+        self.maxpool = resnet.maxpool
+        self.layer1 = resnet.layer1
+        self.layer2 = resnet.layer2
+        self.layer3 = resnet.layer3
+        self.layer4 = resnet.layer4
+
+    def forward(self, x):
+        get_ipython().run_line_magic('pinfo2', '')
+        out = self.bn1(out)
+        out = F.relu(out)
+        out = self.maxpool(out)
+        get_ipython().run_line_magic('pinfo2', '')
+        out = F.avg_pool2d(out, kernel_size=7, stride=7)
+
+        return out
+
+# Set our model with pre-trained model 
+resnet = ResNet().cuda()
+
+
+# In[ ]:
+
+
+# Extract ConvNet Features (VGG19, ResNet)
+def extract_deep_features(path, feature_extractor, feature_size):
+    start_time = time.time()
+    
+    list_imgs_names = os.listdir(path) #list_imgs_names
+    N = len(list_imgs_names)
+    feature_all = np.zeros((N, feature_size)) # create an array to store features
+    image_all = [] # define empy array to store image names
+    
+    transform = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    
+    # extract features 
+    for index, img_name in enumerate(list_imgs_names):
+        img_path = os.path.join(path, img_name)
+        
+        # Image Read & Resize
+        image_np = Image.open(img_path) # Read the images
+        image_np = np.array(image_np)
+        image_np = resize(image_np, (224, 224), mode='constant') # Resize the images
+        image_np = torch.from_numpy(image_np).permute(2, 0, 1).float()
+        image_np = transform(image_np)
+        image_np = Variable(image_np.unsqueeze(0))   #bs, c, h, w
+        image_np = image_np.cuda()
+        
+        # Extract Feature
+        feature = feature_extractor(image_np)
+        feature = feature.squeeze().cpu().data.numpy()
+        feature = feature.reshape((1, feature_size)) # Feature Flatten
+        feature = feature / LA.norm(feature) # Feature Normalization
+        feature_all[index] = feature
+        image_all.append(img_name)
+
+    time_elapsed = time.time() - start_time
+
+    print('Feature extraction complete in {:.02f}s'.format(time_elapsed % 60))
+
+    return feature_all, image_all
+
+
+# In[ ]:
+
+
+def test_deep_feature(feature_extractor, feature_size):
+    # Extract features from the dataset
+    print('Extract features from data')
+    path = './data'
+    feats, image_list = extract_deep_features(path, feature_extractor, feature_size=feature_size)
+
+    # test image path
+    print('Extract features from query image')
+    test = './test'
+    feat_single, image = extract_deep_features(test, feature_extractor, feature_size=feature_size)
+    
+    # Calculate the scores
+    scores  = np.dot(feat_single, feats.T)
+    sort_ind = np.argsort(scores)[0][::-1] # sort the scores
+    scores = scores[0, sort_ind]
+
+    # Show the results
+    maxres = 10
+    imlist = [image_list[index] for i, index in enumerate(sort_ind[0:maxres])]
+    print ("top %d images in order are: " %maxres, imlist)
+
+    fig=plt.figure(figsize=(16, 10))
+    for i in range(len(imlist)):
+        sample = imlist[i]
+        img = mpimg.imread('./data' + '/' + sample)
+        ax = fig.add_subplot(2, 5, i+1)
+        ax.autoscale()
+        plt.tight_layout()
+        plt.imshow(img, interpolation='nearest')
+        ax.set_title('{:.3f}%'.format(scores[i]))
+        ax.axis('off')
+    plt.show()
+
+
+# In[ ]:
+
+
+# VGG19 Image Retrieval Results
+test_deep_feature(vgg19, feature_size=4096)
+
+
+# In[ ]:
+
+
+# ResNet50 Image Retrieval Results
+test_deep_feature(resnet, feature_size=2048)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ 2_face_detection.py
@@ -0,0 +1,559 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Face Detection 실습
+
+# In[1]:
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+import math
+import numpy as np
+from collections import OrderedDict
+from PIL import Image, ImageDraw, ImageFont
+
+from box_utils import nms, calibrate_box, get_image_boxes, convert_to_square, _preprocess
+os.environ['CUDA_VISIBLE_DEVICES']='1'
+
+
+# ## MTCNN Model
+
+# ![MTCNN1](image/MTCNN1.png)
+
+# ![MTCNN2](image/MTCNN2.png)
+
+# In[2]:
+
+
+class Flatten(nn.Module):
+
+    def __init__(self):
+        super(Flatten, self).__init__()
+
+    def forward(self, x):
+        """
+        Arguments:
+            x: a float tensor with shape [batch_size, c, h, w].
+        Returns:
+            a float tensor with shape [batch_size, c*h*w].
+        """
+
+        # without this pretrained model isn't working
+        x = x.transpose(3, 2).contiguous()
+
+        return x.view(x.size(0), -1)
+
+
+# ## P-Net
+
+# In[3]:
+
+
+class PNet(nn.Module):
+    def __init__(self):
+        super(PNet, self).__init__()
+        # suppose we have input with size HxW, then
+        # after first layer: H - 2,
+        # after pool: ceil((H - 2)/2),
+        # after second conv: ceil((H - 2)/2) - 2,
+        # after last conv: ceil((H - 2)/2) - 4,
+        # and the same for W
+
+        self.features = nn.Sequential(OrderedDict([
+            ('conv1', nn.Conv2d(3, 10, 3, 1)),
+            ('prelu1', nn.PReLU(10)),
+            ('pool1', nn.MaxPool2d(2, 2, ceil_mode=True)),
+
+            ('conv2', nn.Conv2d(10, 16, 3, 1)),
+            ('prelu2', nn.PReLU(16)),
+
+            ('conv3', nn.Conv2d(16, 32, 3, 1)),
+            ('prelu3', nn.PReLU(32))
+        ]))
+
+        self.conv4_1 = nn.Conv2d(32, 2, 1, 1)
+        self.conv4_2 = nn.Conv2d(32, 4, 1, 1)
+
+        weights = np.load('weights/pnet.npy', allow_pickle=True)[()]
+        for n, p in self.named_parameters():
+            p.data = torch.FloatTensor(weights[n])
+
+    def forward(self, x):
+        """
+        Arguments:
+            x: a float tensor with shape [batch_size, 3, h, w].
+        Returns:
+            b: a float tensor with shape [batch_size, 4, h', w'].
+            a: a float tensor with shape [batch_size, 2, h', w'].
+        """
+        x = self.features(x)
+        a = self.conv4_1(x)
+        b = self.conv4_2(x) # Bounding Box Regression
+        a = F.softmax(a, dim=1) # Face Classification
+        return b, a
+
+
+# ## R-Net
+
+# In[4]:
+
+
+class RNet(nn.Module):
+    def __init__(self):
+        super(RNet, self).__init__()
+        self.features = nn.Sequential(OrderedDict([
+            ('conv1', nn.Conv2d(3, 28, 3, 1)),
+            ('prelu1', nn.PReLU(28)),
+            ('pool1', nn.MaxPool2d(3, 2, ceil_mode=True)),
+
+            ('conv2', nn.Conv2d(28, 48, 3, 1)),
+            ('prelu2', nn.PReLU(48)),
+            ('pool2', nn.MaxPool2d(3, 2, ceil_mode=True)),
+
+            ('conv3', nn.Conv2d(48, 64, 2, 1)),
+            ('prelu3', nn.PReLU(64)),
+
+            ('flatten', Flatten()),
+            ('conv4', nn.Linear(576, 128)),
+            ('prelu4', nn.PReLU(128))
+        ]))
+
+        self.conv5_1 = nn.Linear(128, 2)
+        self.conv5_2 = nn.Linear(128, 4)
+
+        weights = np.load('weights/rnet.npy', allow_pickle=True)[()]
+        for n, p in self.named_parameters():
+            p.data = torch.FloatTensor(weights[n])
+
+    def forward(self, x):
+        """
+        Arguments:
+            x: a float tensor with shape [batch_size, 3, h, w].
+        Returns:
+            b: a float tensor with shape [batch_size, 4].
+            a: a float tensor with shape [batch_size, 2].
+        """
+        x = self.features(x)
+        a = self.conv5_1(x) 
+        b = self.conv5_2(x) # Bounding Box Regression
+        a = F.softmax(a, dim=1) # Face Classification
+        return b, a
+
+
+# ## O-Net
+
+# In[5]:
+
+
+class ONet(nn.Module):
+    def __init__(self):
+        super(ONet, self).__init__()
+
+        self.features = nn.Sequential(OrderedDict([
+            ('conv1', nn.Conv2d(3, 32, 3, 1)),
+            ('prelu1', nn.PReLU(32)),
+            ('pool1', nn.MaxPool2d(3, 2, ceil_mode=True)),
+
+            ('conv2', nn.Conv2d(32, 64, 3, 1)),
+            ('prelu2', nn.PReLU(64)),
+            ('pool2', nn.MaxPool2d(3, 2, ceil_mode=True)),
+
+            ('conv3', nn.Conv2d(64, 64, 3, 1)),
+            ('prelu3', nn.PReLU(64)),
+            ('pool3', nn.MaxPool2d(2, 2, ceil_mode=True)),
+
+            ('conv4', nn.Conv2d(64, 128, 2, 1)),
+            ('prelu4', nn.PReLU(128)),
+
+            ('flatten', Flatten()),
+            ('conv5', nn.Linear(1152, 256)),
+            ('drop5', nn.Dropout(0.25)),
+            ('prelu5', nn.PReLU(256)),
+        ]))
+
+        self.conv6_1 = nn.Linear(256, 2)
+        self.conv6_2 = nn.Linear(256, 4)
+        self.conv6_3 = nn.Linear(256, 10)
+
+        weights = np.load('weights/onet.npy', allow_pickle=True)[()]
+        for n, p in self.named_parameters():
+            p.data = torch.FloatTensor(weights[n])
+
+    def forward(self, x):
+        """
+        Arguments:
+            x: a float tensor with shape [batch_size, 3, h, w].
+        Returns:
+            c: a float tensor with shape [batch_size, 10].
+            b: a float tensor with shape [batch_size, 4].
+            a: a float tensor with shape [batch_size, 2].
+        """
+        x = self.features(x)
+        a = self.conv6_1(x)
+        b = self.conv6_2(x) # Bounding Box Regression
+        c = self.conv6_3(x) # Face Landmark Detection
+        a = F.softmax(a, dim=1) # Face Classification
+        return c, b, a
+
+
+# ## Run P-Net, Generate Bounding boxes and do NMS
+
+# In[6]:
+
+
+def run_first_stage(image, net, scale, threshold):
+    """Run P-Net, generate bounding boxes, and do NMS.
+
+    Arguments:
+        image: an instance of PIL.Image.
+        net: an instance of pytorch's nn.Module, P-Net.
+        scale: a float number,
+            scale width and height of the image by this number.
+        threshold: a float number,
+            threshold on the probability of a face when generating
+            bounding boxes from predictions of the net.
+
+    Returns:
+        a float numpy array of shape [n_boxes, 9],
+            bounding boxes with scores and offsets (4 + 1 + 4).
+    """
+
+    # scale the image and convert it to a float array
+    width, height = image.size
+    sw, sh = math.ceil(width*scale), math.ceil(height*scale)
+    img = image.resize((sw, sh), Image.BILINEAR)
+    img = np.asarray(img, 'float32')
+
+    with torch.no_grad():
+        img = Variable(torch.FloatTensor(_preprocess(img)))
+        if torch.cuda.is_available():
+            img = img.cuda()
+            net = net.cuda()
+    output = net(img)
+    probs = output[1].data.cpu().numpy()[0, 1, :, :]
+    offsets = output[0].data.cpu().numpy()
+    # probs: probability of a face at each sliding window
+    # offsets: transformations to true bounding boxes
+
+    boxes = _generate_bboxes(probs, offsets, scale, threshold)
+    if len(boxes) == 0:
+        return None
+
+    keep = nms(boxes[:, 0:5], overlap_threshold=0.5)
+    return boxes[keep]
+
+
+def _generate_bboxes(probs, offsets, scale, threshold):
+    """Generate bounding boxes at places
+    where there is probably a face.
+
+    Arguments:
+        probs: a float numpy array of shape [n, m].
+        offsets: a float numpy array of shape [1, 4, n, m].
+        scale: a float number,
+            width and height of the image were scaled by this number.
+        threshold: a float number.
+
+    Returns:
+        a float numpy array of shape [n_boxes, 9]
+    """
+
+    # applying P-Net is equivalent, in some sense, to
+    # moving 12x12 window with stride 2
+    stride = 2
+    cell_size = 12
+
+    # indices of boxes where there is probably a face
+    inds = np.where(probs > threshold)
+
+    if inds[0].size == 0:
+        return np.array([])
+
+    # transformations of bounding boxes
+    tx1, ty1, tx2, ty2 = [offsets[0, i, inds[0], inds[1]] for i in range(4)]
+    # they are defined as:
+    # w = x2 - x1 + 1
+    # h = y2 - y1 + 1
+    # x1_true = x1 + tx1*w
+    # x2_true = x2 + tx2*w
+    # y1_true = y1 + ty1*h
+    # y2_true = y2 + ty2*h
+
+    offsets = np.array([tx1, ty1, tx2, ty2])
+    score = probs[inds[0], inds[1]]
+
+    # P-Net is applied to scaled images
+    # so we need to rescale bounding boxes back
+    bounding_boxes = np.vstack([
+        np.round((stride*inds[1] + 1.0)/scale),
+        np.round((stride*inds[0] + 1.0)/scale),
+        np.round((stride*inds[1] + 1.0 + cell_size)/scale),
+        np.round((stride*inds[0] + 1.0 + cell_size)/scale),
+        score, offsets
+    ])
+    # why one is added?
+
+    return bounding_boxes.T
+
+
+# ## Detect Faces
+
+# In[7]:
+
+
+def detect_faces(image, min_face_size=20.0,
+                 thresholds=[0.6, 0.7, 0.8],
+                 nms_thresholds=[0.7, 0.7, 0.7]):
+    """
+    Arguments:
+        image: an instance of PIL.Image.
+        min_face_size: a float number.
+        thresholds: a list of length 3.
+        nms_thresholds: a list of length 3.
+
+    Returns:
+        two float numpy arrays of shapes [n_boxes, 4] and [n_boxes, 10],
+        bounding boxes and facial landmarks.
+    """
+
+    # LOAD MODELS
+    pnet = PNet()
+    rnet = RNet()
+    onet = ONet()
+    onet.eval()
+
+    # BUILD AN IMAGE PYRAMID
+    width, height = image.size
+    min_length = min(height, width)
+
+    min_detection_size = 12
+    factor = 0.707  # sqrt(0.5)
+
+    # scales for scaling the image
+    scales = []
+
+    # scales the image so that
+    # minimum size that we can detect equals to
+    # minimum face size that we want to detect
+    m = min_detection_size/min_face_size
+    min_length *= m
+
+    factor_count = 0
+    # scales = [0.6, 0.42, 0.30, 0.21, 0.15, 0.10, 0.07, 0.05, 0.03]
+    while min_length > min_detection_size:
+        scales.append(m*factor**factor_count)
+        min_length *= factor
+        factor_count += 1
+
+    # STAGE 1
+
+    # it will be returned
+    bounding_boxes = []
+
+    # run P-Net on different scales
+    for s in scales:
+        boxes = run_first_stage(image, pnet, scale=s, threshold=thresholds[0])
+        bounding_boxes.append(boxes)
+
+    # collect boxes (and offsets, and scores) from different scales
+    bounding_boxes = [i for i in bounding_boxes if i is not None]
+    bounding_boxes = np.vstack(bounding_boxes)
+
+    keep = nms(bounding_boxes[:, 0:5], nms_thresholds[0]) # NMS (Non-Maximum-Suppression)
+    bounding_boxes = bounding_boxes[keep]
+
+    # use offsets predicted by pnet to transform bounding boxes
+    bounding_boxes = calibrate_box(bounding_boxes[:, 0:5], bounding_boxes[:, 5:])
+    # shape [n_boxes, 5]
+
+    bounding_boxes = convert_to_square(bounding_boxes)
+    bounding_boxes[:, 0:4] = np.round(bounding_boxes[:, 0:4])
+
+    # STAGE 2
+
+    img_boxes = get_image_boxes(bounding_boxes, image, size=24)
+    with torch.no_grad():
+        img_boxes = Variable(torch.FloatTensor(img_boxes))
+        if torch.cuda.is_available():
+            rnet = rnet.cuda()
+            img_boxes = img_boxes.cuda()
+    output = rnet(img_boxes)
+    offsets = output[0].data.cpu().numpy()  # shape [n_boxes, 4]
+    probs = output[1].data.cpu().numpy()  # shape [n_boxes, 2]
+
+    keep = np.where(probs[:, 1] > thresholds[1])[0]
+    bounding_boxes = bounding_boxes[keep]
+    bounding_boxes[:, 4] = probs[keep, 1].reshape((-1,))
+    offsets = offsets[keep]
+
+    keep = nms(bounding_boxes, nms_thresholds[1]) # NMS (Non-Maximum-Suppression)
+    bounding_boxes = bounding_boxes[keep]
+    bounding_boxes = calibrate_box(bounding_boxes, offsets[keep])
+    bounding_boxes = convert_to_square(bounding_boxes)
+    bounding_boxes[:, 0:4] = np.round(bounding_boxes[:, 0:4])
+
+    # STAGE 3
+
+    img_boxes = get_image_boxes(bounding_boxes, image, size=48)
+    if len(img_boxes) == 0:
+        return [], []
+    with torch.no_grad():
+        img_boxes = Variable(torch.FloatTensor(img_boxes))
+        if torch.cuda.is_available():
+            onet = onet.cuda()
+            img_boxes = img_boxes.cuda()
+    output = onet(img_boxes)
+    landmarks = output[0].data.cpu().numpy()  # shape [n_boxes, 10]
+    offsets = output[1].data.cpu().numpy()  # shape [n_boxes, 4]
+    probs = output[2].data.cpu().numpy()  # shape [n_boxes, 2]
+
+    keep = np.where(probs[:, 1] > thresholds[2])[0]
+    bounding_boxes = bounding_boxes[keep]
+    bounding_boxes[:, 4] = probs[keep, 1].reshape((-1,))
+    offsets = offsets[keep]
+    landmarks = landmarks[keep]
+
+    # compute landmark points
+    width = bounding_boxes[:, 2] - bounding_boxes[:, 0] + 1.0
+    height = bounding_boxes[:, 3] - bounding_boxes[:, 1] + 1.0
+    xmin, ymin = bounding_boxes[:, 0], bounding_boxes[:, 1]
+    landmarks[:, 0:5] = np.expand_dims(xmin, 1) + np.expand_dims(width, 1)*landmarks[:, 0:5]
+    landmarks[:, 5:10] = np.expand_dims(ymin, 1) + np.expand_dims(height, 1)*landmarks[:, 5:10]
+
+    bounding_boxes = calibrate_box(bounding_boxes, offsets)
+    keep = nms(bounding_boxes, nms_thresholds[2], mode='min') # NMS (Non-Maximum-Suppression)
+    bounding_boxes = bounding_boxes[keep]
+    landmarks = landmarks[keep]
+
+    return bounding_boxes, landmarks
+
+
+# ## Visualization
+
+# In[8]:
+
+
+def show_bboxes(img, bounding_boxes, facial_landmarks=[]):
+    """Draw bounding boxes and facial landmarks.
+
+    Arguments:
+        img: an instance of PIL.Image.
+        bounding_boxes: a float numpy array of shape [n, 5].
+        facial_landmarks: a float numpy array of shape [n, 10].
+
+    Returns:
+        an instance of PIL.Image.
+    """
+
+    img_copy = img.copy()
+    draw = ImageDraw.Draw(img_copy)
+
+    # Draw Bounding boxes
+    for b in bounding_boxes:
+        draw.rectangle([
+            (b[0], b[1]), (b[2], b[3])
+        ], outline='white')
+
+    # Draw Facial Landmarks
+    for p in facial_landmarks:
+        for i in range(5):
+            draw.ellipse([
+                (p[i] - 1.0, p[i + 5] - 1.0),
+                (p[i] + 1.0, p[i + 5] + 1.0)
+            ], outline='blue')
+
+    return img_copy
+
+
+# ## Results
+
+# In[9]:
+
+
+img = Image.open('image/office1.jpg')
+
+bounding_boxes, landmarks = detect_faces(img)
+show_bboxes(img, bounding_boxes, landmarks)
+
+
+# In[10]:
+
+
+img = Image.open('image/office2.jpg')
+
+bounding_boxes, landmarks = detect_faces(img)
+show_bboxes(img, bounding_boxes, landmarks)
+
+
+# In[11]:
+
+
+img = Image.open('image/office3.jpg')
+
+bounding_boxes, landmarks = detect_faces(img)
+show_bboxes(img, bounding_boxes, landmarks)
+
+
+# In[12]:
+
+
+img = Image.open('image/office4.jpg')
+
+bounding_boxes, landmarks = detect_faces(img)
+show_bboxes(img, bounding_boxes, landmarks)
+
+
+# In[13]:
+
+
+img = Image.open('image/office5.jpg')
+
+bounding_boxes, landmarks = detect_faces(img)
+show_bboxes(img, bounding_boxes, landmarks)
+
+
+# In[14]:
+
+
+img = Image.open('image/image1.jpg')
+
+bounding_boxes, landmarks = detect_faces(img)
+show_bboxes(img, bounding_boxes, landmarks)
+
+
+# In[15]:
+
+
+img = Image.open('image/image2.jpg')
+
+bounding_boxes, landmarks = detect_faces(img)
+show_bboxes(img, bounding_boxes, landmarks)
+
+
+# In[16]:
+
+
+img = Image.open('image/image3.jpg')
+
+bounding_boxes, landmarks = detect_faces(img)
+show_bboxes(img, bounding_boxes, landmarks)
+
+
+# In[17]:
+
+
+img = Image.open('image/image4.jpg')
+
+bounding_boxes, landmarks = detect_faces(img)
+show_bboxes(img, bounding_boxes, landmarks)
+
+
+# In[ ]:
+
+
+
+
